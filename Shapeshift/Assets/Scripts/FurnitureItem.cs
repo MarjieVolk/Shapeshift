7aaad212e66ebc3cc3147b0831c08d860fff66cd
Marjie <marjie.volk@gmail.com>
2016-04-16 13:53:41 -0700

    Some skeletal stuff

---
+++ Shapeshift/Assets/Scripts/FurnitureItem.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FurnitureItem : MonoBehaviour {
+
+    Sprite image;
+    int width;
+    int height;
+
+    RoomType[] roomTypes;
+
+	// Use this for initialization
+	protected void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	protected void Update () {
+	
+	}
+}
---
+++ Shapeshift/Assets/Scripts/PlayableFurnitureItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class PlayableFurnitureItem : FurnitureItem {
+
+    int quality = 0;
+
+    // Use this for initialization
+    new void Start() {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    new void Update() {
+        base.Update();
+    }
+}
