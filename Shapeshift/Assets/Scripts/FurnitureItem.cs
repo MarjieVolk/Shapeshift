ddfc95b86bcc109f68fc364113d6b4b76fb86bda
nelk <nelkishere@gmail.com>
2016-04-16 15:07:44 -0700

    TileItem and snapping to grid at start.

--- Shapeshift/Assets/Scripts/FurnitureItem.cs
+++ Shapeshift/Assets/Scripts/FurnitureItem.cs
@@ -1,21 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof (TileItem))]
 public class FurnitureItem : MonoBehaviour {
 
     public Sprite image;
     public int width;
     public int height;
 
+
     public RoomType[] roomTypes;
 
-	// Use this for initialization
-	protected void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	protected void Update () {
-	
-	}
+    // Use this for initialization
+    protected void Start () {
+
+    }
+
+
+    // Update is called once per frame
+    protected void Update () {
+
+    }
 }
