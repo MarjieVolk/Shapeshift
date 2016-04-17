f67e8a2616d57d579badeb0f454da4293c6ea3d6
Emma Breen <emmabreen@Emmas-MacBook-Pro.local>
2016-04-16 18:26:51 -0700

    Added more guard stuff

---
+++ Shapeshift/Assets/Scripts/GuardDuty.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuardDuty : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public IEnumerable getWaypoints()
+	{
+		return new Waypoints ();
+	}
+
+	public class Waypoints : IEnumerable
+	{
+		public IEnumerator GetEnumerator()
+		{
+			yield return 1;
+		}
+	}
+}
