cd177ac654cce0b798eac0fbc5c2faa94c1eb1d7
Marjie <marjie.volk@gmail.com>
2016-04-16 21:57:01 -0700

    Fixing up demo scene

--- Shapeshift/Assets/Scripts/CameraController.cs
+++ Shapeshift/Assets/Scripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour {
     private const float SCALE = 1.0f;
 
     private TileItem playerTileItem;
+    private Room previousRoom;
+    private Room currentRoom;
 
 	// Use this for initialization
 	void Start () {
@@ -14,16 +16,28 @@ public class CameraController : MonoBehaviour {
 
         Camera.main.orthographicSize = Screen.height / (100.0f * 2.0f * SCALE);
         Debug.Log("Screen height: " + Screen.height + "  Camera size: " + Camera.main.orthographicSize);
-	}
+
+        previousRoom = getCurrentRoom();
+        transform.position = previousRoom.transform.position;
+    }
 
     void Update() {
+        Room room = getCurrentRoom();
+        
+        if (room != previousRoom) {
+
+        }
+
+        previousRoom = room;
+    }
+
+    private Room getCurrentRoom() {
         List<Room> potentialRooms = TileItem.GetObjectsAtPosition<Room>(playerTileItem.tileX, playerTileItem.tileY);
 
         if (potentialRooms.Count != 1) {
             Debug.LogError("Found " + potentialRooms.Count + " rooms at (" + playerTileItem.tileX + ", " + playerTileItem.tileY + ")");
         }
 
-        Room room = potentialRooms[0];
-        transform.position = room.transform.position;
+        return potentialRooms[0];
     }
 }
