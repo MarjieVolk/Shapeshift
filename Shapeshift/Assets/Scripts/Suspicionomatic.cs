76123ebad7f11fc06b38f3d804b8bc6aac7cecb2
Marjie <marjie.volk@gmail.com>
2016-04-16 19:50:52 -0700

    Added some suspicion analysis stuff

---
+++ Shapeshift/Assets/Scripts/Suspicionomatic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Suspicionomatic {
+
+    public static int getSuspicionLevel(FurnitureItem item) {
+        int suspicionLevel = 0;
+
+        // +1 suspicion level per item in the same room that does not share a room type
+        TileItem itemTileItem = item.GetComponent<TileItem>();
+        List<Room> potentialRooms = TileItem.GetObjectsAtPosition<Room>(itemTileItem.tileX, itemTileItem.tileY);
+
+        if (potentialRooms.Count != 1) {
+            Debug.LogError("Found " + potentialRooms.Count + " rooms at (" + itemTileItem.tileX + ", " + itemTileItem.tileY + ")");
+        }
+        
+        foreach (FurnitureItem other in potentialRooms[0].getAllFurniture()) {
+            if (other != item) {
+                bool hasRoomTypeMatch = false;
+                foreach (RoomType itemRoomType in item.roomTypes) {
+                    foreach (RoomType otherRoomType in other.roomTypes) {
+                        if (itemRoomType == otherRoomType) {
+                            hasRoomTypeMatch = true;
+                            break;
+                        }
+                    }
+                }
+
+                if (!hasRoomTypeMatch) {
+                    suspicionLevel++;
+                }
+            }
+        }
+
+        // + suspicion level for each broken placement rule
+        // TODO
+
+        return suspicionLevel;
+    }
+}
