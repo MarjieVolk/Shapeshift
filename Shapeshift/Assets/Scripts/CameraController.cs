48c5b80a4ef31857ade5c18431ad59c2818e3107
Marjie <marjie.volk@gmail.com>
2016-04-16 21:04:44 -0700

    Well camera controller does something

---
+++ Shapeshift/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraController : MonoBehaviour {
+
+    private const float SCALE = 1.0f;
+
+    private TileItem playerTileItem;
+
+	// Use this for initialization
+	void Start () {
+        playerTileItem = FindObjectOfType<PlayerController>().gameObject.GetComponent<TileItem>();
+
+        Camera.main.orthographicSize = Screen.height / (100.0f * 2.0f * SCALE);
+        Debug.Log("Screen height: " + Screen.height + "  Camera size: " + Camera.main.orthographicSize);
+	}
+
+    void Update() {
+        List<Room> potentialRooms = TileItem.GetObjectsAtPosition<Room>(playerTileItem.tileX, playerTileItem.tileY);
+
+        if (potentialRooms.Count != 1) {
+            Debug.LogError("Found " + potentialRooms.Count + " rooms at (" + playerTileItem.tileX + ", " + playerTileItem.tileY + ")");
+        }
+
+        Room room = potentialRooms[0];
+        transform.position = room.transform.position;
+    }
+}
