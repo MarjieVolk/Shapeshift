b437c3f22bebd4d462418cbe9764f58ebb8fcb54
Emma Breen <emmabreen@Emmas-MacBook-Pro.local>
2016-04-16 20:49:38 -0700

    Adds a Direction enum and more unfinished guard behavior.

--- Shapeshift/Assets/Scripts/Guard.cs
+++ Shapeshift/Assets/Scripts/Guard.cs
@@ -6,6 +6,10 @@ using System.Collections.Generic;
 [RequireComponent (typeof (TileItem))]
 public class Guard : MonoBehaviour {
 
+	public float Speed = 0.01f;
+	public int LookTime = 200;
+	public Direction FirstDirection = Direction.SOUTH;
+
 	private GuardWaypoint[] waypoints;
 	private GuardWaypoint currentWaypoint;
 	private GuardAction currentAction;
@@ -16,6 +20,14 @@ public class Guard : MonoBehaviour {
 		CATCH
 	}
 
+	// Put data for MOVE here.
+	private int goalTileX;
+	private int goalTileY;
+
+	// Put data for LOOK here.
+	private Direction currentDirection;
+	private int lookTimer;
+
 	// Use this for initialization
 	void Start () {
 		// A Guard must have GuardDuty as a parent.
@@ -23,13 +35,44 @@ public class Guard : MonoBehaviour {
 		if (waypoints.Length > 0) {
 			currentWaypoint = GetNextWaypoint (Int32.MinValue);
 		}
-		currentAction = GuardAction.LOOK;
+
+		// Guard looks around before anything else.
+		InitializeLook();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y + 0.00f);
-		//GetComponent<TileItem>().SetGlobalPosition(new Vector3(transform.position.x + 1, transform.position.y + 1));
+		if (currentAction == GuardAction.LOOK) {
+			UpdateLook ();
+		}
+		GetComponent<TileItem>().SetGlobalPosition(new Vector3(transform.position.x + Speed, transform.position.y + Speed));
+	}
+
+	// MOVE initialization.
+	void InitializeMove() {
+		currentAction = GuardAction.MOVE;
+	}
+
+	// LOOK initialization.
+	void InitializeLook() {
+		currentAction = GuardAction.LOOK;
+		lookTimer = LookTime;
+		currentDirection = FirstDirection;
+	}
+
+	// Update in LOOK mode.
+	void UpdateLook() {
+		if (lookTimer <= 0) {
+			currentDirection = Clockwise (currentDirection);
+			if (currentDirection == FirstDirection) {
+				InitializeMove ();
+			} else {
+				lookTimer = LookTime;
+			}
+		} else {
+			lookTimer -= 1;
+		}
+			
 	}
 
 	// Grabs the guard's next waypoint.
@@ -48,6 +91,18 @@ public class Guard : MonoBehaviour {
 			return nextWaypoint;
 		} else {
 			return GetNextWaypoint (Int32.MinValue);
+		}
+	}
+
+	Direction Clockwise(Direction fromMe) {
+		if (fromMe == Direction.NORTH) {
+			return Direction.EAST;
+		} else if (fromMe == Direction.EAST) {
+			return Direction.SOUTH;
+		} else if (fromMe == Direction.SOUTH) {
+			return Direction.WEST;
+		} else {
+			return Direction.NORTH;
 		}
 	}
 }
