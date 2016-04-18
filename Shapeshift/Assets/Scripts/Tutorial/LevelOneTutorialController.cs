4c7fca9f77971969cbb1e34a173b3732e12fb654
Marjie <marjie.volk@gmail.com>
2016-04-17 21:04:03 -0700

    Started doing a tutorial in Level 1

---
+++ Shapeshift/Assets/Scripts/Tutorial/LevelOneTutorialController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelOneTutorialController : MonoBehaviour {
+
+    public Collider2D bathroom;
+
+    public TutorialText tutorialTextPrefab;
+    public float popupDelayTime = 3;
+
+    private bool shiftPopupDone = false;
+    private bool playerHasTransformed = false;
+
+    private float matchRoomPopupTriggeredTime = -1;
+    private bool matchRoomPopupDone = false;
+
+    private Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+        canvas = GameObject.FindObjectOfType<Canvas>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bathroom.on
+	    if (!shiftPopupDone && Time.time >= popupDelayTime) {
+            createShiftPopup();
+        }
+
+        if (!matchRoomPopupDone && matchRoomPopupTriggeredTime != -1 && Time.time - matchRoomPopupTriggeredTime >= popupDelayTime) {
+            createMatchRoomPopup();
+        }
+	}
+
+    private void createShiftPopup() {
+        shiftPopupDone = true;
+
+        TutorialText shiftPopup = init();
+        shiftPopup.setText("To shift your shape, click one of the buttons on your toolbar.");
+        shiftPopup.addCloseCondition(() => {
+            return playerHasTransformed;
+        });
+        shiftPopup.addCloseHandler(() => {
+            matchRoomPopupTriggeredTime = Time.time;
+        });
+
+        GameObject.FindObjectOfType<PlayerTransformer>().PlayerTransformed += (GameObject target) => {
+            playerHasTransformed = true;
+        };
+    }
+
+    private void createMatchRoomPopup() {
+        matchRoomPopupDone = true;
+
+        TutorialText popup = init();
+        popup.setText("To hide from guards, transform into a piece of furniture that matches the rest of the room.  "
+            + "For a bathroom, try a sink.");
+    }
+
+    private TutorialText init() {
+        TutorialText popup = Instantiate(tutorialTextPrefab.gameObject).GetComponent<TutorialText>();
+        popup.transform.SetParent(canvas.transform);
+        popup.transform.position += new Vector3(Screen.width / 4, Screen.height / 2);
+        return popup;
+    }
+}
---
+++ Shapeshift/Assets/Scripts/Tutorial/TutorialText.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TutorialText : MonoBehaviour {
+
+    public delegate bool CloseCondition();
+
+    private List<UnityAction> closeActions;
+    private List<CloseCondition> closeConditions;
+
+	// Use this for initialization
+	void Awake () {
+        closeActions = new List<UnityAction>();
+        closeConditions = new List<CloseCondition>();
+
+        transform.FindChild("Close Button").GetComponent<Button>().onClick.AddListener(() => {
+            close();
+        });
+	}
+
+    void Update () {
+        foreach (CloseCondition closeCondition in closeConditions) {
+            if (closeCondition()) {
+                close();
+                break;
+            }
+        }
+    }
+
+    public void setText(string text) {
+        transform.FindChild("Text").GetComponent<Text>().text = text;
+    }
+
+    public void addCloseCondition(CloseCondition condition) {
+        closeConditions.Add(condition);
+    }
+
+    public void addCloseHandler(UnityAction action) {
+        closeActions.Add(action);
+    }
+
+    public void close() {
+        foreach (UnityAction action in closeActions) {
+            action();
+        }
+
+        Destroy(gameObject);
+    }
+}
