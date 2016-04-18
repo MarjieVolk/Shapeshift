2970b8d5b4b00d62b49dbda2d5d678ae65457802
Marjie <marjie.volk@gmail.com>
2016-04-17 21:07:54 -0700

    CollisionEventCommunicator.  It's the shit

---
+++ Shapeshift/Assets/Scripts/CollisionEventCommunicator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class CollisionEventCommunicator : MonoBehaviour {
+    
+    public event UnityAction OnCollisionEnter;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D() {
+        OnCollisionEnter();
+    }
+}
