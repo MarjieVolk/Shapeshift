9e9dc4c780896b1037722fa04903234d2c09a5c5
Marjie <marjie.volk@gmail.com>
2016-04-18 11:19:11 -0700

    Make guards catch player when they collide while in chase state

--- Shapeshift/Assets/Scripts/Guards/ChaseState.cs
+++ Shapeshift/Assets/Scripts/Guards/ChaseState.cs
@@ -68,4 +68,11 @@ public class ChaseState : State {
 			StartChase ();
 		}
 	}
+
+    public void OnTriggerEnter2D(Collider2D other) {
+        PlayerController player = other.GetComponent<PlayerController>();
+        if (player != null) {
+            player.catchPlayer();
+        }
+    }
 }
