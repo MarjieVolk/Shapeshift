eaf40af75333c10790aec1b76b67efe2eeed1727
Taylor Bergquist <knexer@gmail.com>
2016-04-17 18:58:17 -0700

    Pull guard code into separate move and look states.

---
+++ Shapeshift/Assets/Scripts/State.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class State : MonoBehaviour {
+
+    private int lookTimer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Shapeshift/Assets/Scripts/StateMachine.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// A class for managing state-machine-based AIs
+/// </summary>
+public class StateMachine : MonoBehaviour {
+
+    public State initialState;
+
+    private State _currentState;
+    public State CurrentState
+    {
+        get { return _currentState; }
+        set
+        {
+            if (_currentState != null)
+            {
+                _currentState.enabled = false;
+            }
+            _currentState = value;
+            _currentState.enabled = true;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        foreach (State state in GetComponents<State>())
+        {
+            state.enabled = false;
+        }
+        CurrentState = initialState;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
